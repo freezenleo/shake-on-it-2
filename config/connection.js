69cb80dc5f4b7a27c5b5654e33b654a082565025
Clark <81452611+freezenleo@users.noreply.github.com>
2021-08-26 15:23:23 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,6 @@ var mysql = require("mysql2");
 
 let sequelize;
 
-console.log(process.env.JAWSDB_URL);
-console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW);
-
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
